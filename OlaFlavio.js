d14a171e83231cb6ae6ff7df79de2469855f5717
aluno cmc <alunocmc@umc.br>
2026-03-10 21:18:47 -0300

    Terceiro commit, falando sobre mim

--- OlaFlavio.js
+++ OlaFlavio.js
@@ -1,7 +1,25 @@
 <div>
-    <p>Olá, mundo!</p>
-</div>
-
-<p>lorem
-    
-</p>
+    <h1>Muito prazer, sou o Kauã!</h1>
+  <p>
+    Minha paixão pela tecnologia não é de hoje; ela me acompanha desde a
+    infância. Sempre fui fascinado por entender como as coisas funcionam por
+    trás das telas. Hoje, dedico essa mesma curiosidade e energia para
+    transformar conceitos visuais em interfaces funcionais, dinâmicas e
+    estritamente centradas na experiência do usuário. Iniciei minha jornada
+    profissional cedo, em 2022, como Jovem Aprendiz aos 19 anos na Nova Presto,
+    onde consolidei minha base em desenvolvimento web e boas práticas de Clean
+    Code. Essa vivência inicial foi fundamental para que eu assumisse, em 2024,
+    o cargo de Desenvolvedor Front-end na SMJ CodeSystem, focando na criação de
+    sistemas de gerenciamento complexos e interfaces responsivas. Minha
+    especialidade reside no ecossistema JavaScript. Tenho versatilidade para
+    arquitetar soluções tanto com React.js quanto com Vue.js, aliado à agilidade
+    do Tailwind CSS para entregas acessíveis e visualmente impecáveis. Além de
+    dominar o consumo de APIs RESTful, fiz questão de desenvolver conhecimentos
+    introdutórios em Back-end. Essa visão sistêmica me permite dialogar com
+    clareza com toda a equipe técnica e propor soluções integradas mais
+    inteligentes. Sou um profissional movido pelo aprendizado constante.
+    Atualmente, estou cursando Análise e Desenvolvimento de Sistemas pela
+    Universidade Mogi das Cruzes, fortalecendo minha base teórica para continuar
+    criando produtos digitais que geram impacto real e valor para os usuários.
+  </p>
+</div>;
